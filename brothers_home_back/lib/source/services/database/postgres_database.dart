05c80e91bfe0cb7033d7c365ce792cfa750bd22a
Marcelo Nascimento <marcelohenrique7x@gmail.com>
2022-08-11 17:52:10 -0300

    database att

---
+++ brothers_home_back/lib/source/services/database/postgres_database.dart
@@ -0,0 +1,49 @@
+import 'dart:async';
+import 'package:brothers_home/source/services/database/remote_database_interface.dart';
+import 'package:postgres/postgres.dart';
+import 'package:shelf_modular/shelf_modular.dart';
+import '../dot_env.dart';
+
+class PostgresDatabase implements RemoteDatabase, Disposable {
+  final completer = Completer<PostgreSQLConnection>();
+  final DotEnvService dotEnv;
+
+  PostgresDatabase(this.dotEnv) {
+    _init();
+  }
+
+  _init() async {
+    final url = dotEnv['DATABASE_URL']!;
+
+    final uri = Uri.parse(url);
+
+    var connection = PostgreSQLConnection(
+      uri.host,
+      uri.port,
+      uri.pathSegments.first,
+      username: uri.userInfo.split(':').first,
+      password: uri.userInfo.split(':').last,
+    );
+    await connection.open();
+    completer.complete(connection);
+  }
+
+  @override
+  Future<List<Map<String, Map<String, dynamic>>>> query(
+    String queryText, {
+    Map<String, dynamic> variables = const {},
+  }) async {
+    final connection = await completer.future;
+
+    return await connection.mappedResultsQuery(
+      queryText,
+      substitutionValues: variables,
+    );
+  }
+
+  @override
+  void dispose() async {
+    final connection = await completer.future;
+    await connection.close();
+  }
+}
