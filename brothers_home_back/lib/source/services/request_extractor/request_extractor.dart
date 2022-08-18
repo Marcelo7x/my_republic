5283115d13287e5caf7b672961eb2283aa941441
Marcelo Nascimento <marcelohenrique7x@gmail.com>
2022-08-18 20:42:25 -0300

    requestExtractor

---
+++ brothers_home_back/lib/source/services/request_extractor/request_extractor.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'dart:html';
+
+import 'package:shelf/shelf.dart';
+
+class RequestExtractor {
+  String getAuthorizationBearer(Request request) {
+    var authorization = request.headers['autorization'] ?? '';
+    authorization = authorization.split(' ').last;
+    return authorization;
+  }
+
+  LoginCredentials getAuthorizationBasic(Request request) {
+    var authorization = request.headers['autorization'] ?? '';
+    authorization = authorization.split(' ').last;
+    authorization = String.fromCharCodes(base64Decode(authorization));
+
+    final credentials = authorization.split(':');
+    return LoginCredentials(email: credentials.first, password: credentials.last);
+  }
+}
+
+class LoginCredentials {
+  final String email;
+  final String password;
+
+  LoginCredentials({required this.email, required this.password});
+}
