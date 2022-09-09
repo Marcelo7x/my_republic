e9770f3b43210148a825f2005ac2b412491be6e3
Marcelo Nascimento <marcelohenrique7x@gmail.com>
2022-09-08 23:43:21 -0300

    tentativa de consertar bug postgres

--- backend/lib/source/core/services/database/postgres_database.dart
+++ backend/lib/source/core/services/database/postgres_database.dart
@@ -17,16 +17,13 @@ class PostgresDatabase implements RemoteDatabase, Disposable {
 
     final uri = Uri.parse(url);
 
-    // final uri = Uri.parse('postgres://postgres:postgres@postgres:5432/myrepublic');
-    // final uri = Uri.parse('postgres://dsmfeihxhfgtda:3db2772d5f30229c87af9db8f16ed03d5f72c6212eab24a12d75425eddb823c6@ec2-52-3-200-138.compute-1.amazonaws.com:5432/d9o2kdm4pijg4k');
-
     var connection = PostgreSQLConnection(
       uri.host,
       uri.port,
       uri.pathSegments.first,
       username: uri.userInfo.split(':').first,
       password: uri.userInfo.split(':').last,
-      useSSL: uri.host == 'postgres'? false:true,
+      useSSL: uri.host == 'postgres' ? false : true,
     );
     await connection.open();
     completer.complete(connection);
@@ -37,12 +34,25 @@ class PostgresDatabase implements RemoteDatabase, Disposable {
     String queryText, {
     Map<String, dynamic> variables = const {},
   }) async {
-    final connection = await completer.future;
+    bool erroConnection = false;
+    do {
+      final connection = await completer.future;
 
-    return await connection.mappedResultsQuery(
-      queryText,
-      substitutionValues: variables,
-    );
+      try {
+        return await connection.mappedResultsQuery(
+          queryText,
+          substitutionValues: variables,
+        );
+      } on PostgreSQLException catch (e) {
+        if (!erroConnection) {
+          erroConnection = true;
+          _init();
+          continue;
+        } else {
+          rethrow;
+        }
+      }
+    } while (erroConnection);
   }
 
   @override
