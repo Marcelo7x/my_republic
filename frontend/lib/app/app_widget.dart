53868ad1c822a1a29822e53816926dbff2a3c777
Marcelo Nascimento <marcelohenrique7x@gmail.com>
2022-11-12 02:47:43 -0300

    -

--- frontend/lib/app/app_widget.dart
+++ frontend/lib/app/app_widget.dart
@@ -1,8 +1,11 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'colors/color_schemes.g.dart';
 
-ValueNotifier<Map<String, dynamic>> themeModeAndColor = ValueNotifier({'thememode': ThemeMode.light, 'color':Color(0xff2196f3)});
+ValueNotifier<Map<String, dynamic>> themeModeAndColor =
+    ValueNotifier({'thememode': ThemeMode.light, 'color': Color(0xff2196f3)});
 // ValueNotifier<Color> screenPickerColor = ValueNotifier(Color(0xff2196f3));
 
 class AppWidget extends StatelessWidget {
@@ -11,7 +14,7 @@ class AppWidget extends StatelessWidget {
     return ValueListenableBuilder<Map<String, dynamic>>(
       valueListenable: themeModeAndColor,
       builder: (context, value, child) => MaterialApp.router(
-        title: 'Brothers Home',
+        title: 'My Republic',
         routeInformationParser: Modular.routeInformationParser,
         routerDelegate: Modular.routerDelegate,
         themeMode: value['thememode'],
